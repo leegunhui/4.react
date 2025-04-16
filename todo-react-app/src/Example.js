cc919e76a1d9fc1689eb65ac4cddab0f8b0b8b46
leegunhui <luluwin0099@gmail.com>
2025-04-16 18:10:10 +0900

    0416

--- todo-react-app/src/Example.js
+++ todo-react-app/src/Example.js
@@ -2,11 +2,16 @@ import { useState } from "react"
 
 export let Example = () => {
     const [message, setMessage] = useState('Hello,world');
+    const [name,setName] = useState("홍길동");
+    const [count, setCount] = useState(0);
     return(
         <div>
-            <p>{message}</p>
+            <p>{message}/{name}</p>
             {/*상태는 무조건 setter를 이용하여 변경해야한다.*/}
             <button onClick={() => setMessage('Hello, React!')}>Change Message</button>
+
+            <p>Count: {count}</p>
+            <button onClick={() =>setCount(count+1)}>Increase</button>
         </div>
     )
 }
